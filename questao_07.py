4f1b5c2458975f23e0f429dcb6dffffdf7a13bf2
lucarl07 <junitinho2007@gmail.com>
2025-04-25 10:37:50 -0300

    ✅ Efetuar questão 7 com sucesso

---
+++ questao_07.py
@@ -0,0 +1,17 @@
+# QUESTÃO 07:
+# Faça um programa que leia uma string e dois caracteres. Troque todas as ocorrências do primeiro caracter pelo segundo. Exemplo: Seja a string "maracatu" e os caracteres 'a' e 'o', então a string ficará "morocotu".
+# FORMATO DE ENTRADA: Você receberá 03 linhas. Primeira linha: a string; segunda linha: o primeiro caracter; terceira linha: o segundo caracter. A string possui no máximo 100 caracteres.
+# FORMATO DE SAÍDA: Imprima a palavra resultante da substituição dos caracteres na string original.
+
+def substituir(string):
+  charAntigo = input('Digite o caractere que deseja substituir: ')
+  charNovo = input('Digite o caractere que vai substitui-lo: ')
+  return string.replace(charAntigo, charNovo)
+
+textoInicial = input('Digite o texto inicial: ')
+
+if len(textoInicial) > 100:
+  print('[ERRO] Seu texto ultrapassou 100 caracteres. Por favor, tente novamente.')
+else:
+  textoFinal = substituir(textoInicial)
+  print(textoFinal)
